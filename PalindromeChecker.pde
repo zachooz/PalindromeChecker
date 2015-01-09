e1d498204e166e49459e0345477e0dcccaa3dd1a
Zachary Hoffman <zachary.j.hoffman@gmail.com>
2015-01-08 20:33:32 -0800

    Done

--- PalindromeChecker.pde
+++ PalindromeChecker.pde
@@ -1,22 +1,17 @@
-public void setup()
-{
+public void setup(){
   String lines[] = loadStrings("palindromes.txt");
   println("there are " + lines.length + " lines");
-  for (int i=0; i < lines.length; i++) 
-  {
-    if(palindrome(lines[i])==true)
-    {
+  for (int i=0; i < lines.length; i++) {
+    if(palindrome(lines[i])==true){
       println(lines[i] + " IS a palidrome.");
-    }
-    else
-    {
+    }else{
       println(lines[i] + " is NOT a palidrome.");
     }
   }
 }
-public boolean palindrome(String word)
-{
-  //your code here
-  return false;
+public boolean palindrome(String word){
+	if(word.replaceAll("[^a-zA-Z]", "").toLowerCase().equals(new StringBuilder(word).reverse().toString().replaceAll("[^a-zA-Z]", "").toLowerCase())){
+		return true;
+	}
+	return false;
 }
-
---
+++ build-tmp/source/PalindromeChecker.java
@@ -0,0 +1,43 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class PalindromeChecker extends PApplet {
+
+public void setup(){
+  String lines[] = loadStrings("palindromes.txt");
+  println("there are " + lines.length + " lines");
+  for (int i=0; i < lines.length; i++) {
+    if(palindrome(lines[i])==true){
+      println(lines[i] + " IS a palidrome.");
+    }else{
+      println(lines[i] + " is NOT a palidrome.");
+    }
+  }
+}
+public boolean palindrome(String word){
+	if(word.replaceAll("[^a-zA-Z]", "").toLowerCase().equals(new StringBuilder(word).reverse().toString().replaceAll("[^a-zA-Z]", "").toLowerCase())){
+		return true;
+	}
+	return false;
+}
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "PalindromeChecker" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
