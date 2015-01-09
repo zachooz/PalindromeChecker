a8d69a0f0014e47d2abcd6ae6803f85ae589f686
zachooz <hoffking@gmail.com>
2015-01-09 10:37:38 -0800

    Done

--- PalindromeChecker.pde
+++ PalindromeChecker.pde
@@ -2,11 +2,10 @@ public void setup(){
   String lines[] = loadStrings("palindromes.txt");
   println("there are " + lines.length + " lines");
   for (int i=0; i < lines.length; i++) {
-    if(palindrome(lines[i])==true){
+    if(palindrome(lines[i])==true)
       println(lines[i] + " IS a palidrome.");
-    }else{
+    else
       println(lines[i] + " is NOT a palidrome.");
-    }
   }
 }
 public boolean palindrome(String word){
--- build-tmp/source/PalindromeChecker.java
+++ build-tmp/source/PalindromeChecker.java
@@ -18,11 +18,10 @@ public void setup(){
   String lines[] = loadStrings("palindromes.txt");
   println("there are " + lines.length + " lines");
   for (int i=0; i < lines.length; i++) {
-    if(palindrome(lines[i])==true){
+    if(palindrome(lines[i])==true)
       println(lines[i] + " IS a palidrome.");
-    }else{
+    else
       println(lines[i] + " is NOT a palidrome.");
-    }
   }
 }
 public boolean palindrome(String word){
